5e8bd981337c0793eee165d172ab2cc062c7a1d2
Andy White <andywhite441@gmail.com>
2023-05-01 12:16:55 -0600

    Remove CHANGELOG.md

--- release.sh
+++ release.sh
@@ -4,11 +4,8 @@ set -euo pipefail
 echo "What kind of version bump? (major|minor|patch)"
 read version_bump
 
-echo "npm version"
+echo "npm version $version_bump"
 npm version $version_bump
-
-echo "Add new version info to changelog"
-$EDITOR CHANGELOG.md
 
 echo "npm publish"
 npm publish
